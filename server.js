c3fba03e82c6a7713387d80d06079df49116b141
juissi999 <jussi.kaatiala@gmail.com>
2021-09-08 22:48:16 +0300

    Add very simple chat functionality

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const io = new Server(server)
 const httpPort = 3000
 
 io.on('connection', socket=>{
-  console.log(socket.id)
+  console.log("new connection ", socket.id)
+  socket.on('message', (message)=>io.emit('newMessage', socket.id + ': ' + message))
 })
 
 app.get('/', (request, response)=>{
