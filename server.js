01adaaea9f9b7717ebe0bceff4c1099c494459ff
juissi999 <jussi.kaatiala@gmail.com>
2021-09-09 08:37:54 +0300

    Add joined message and message timestamps

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express')
-const app=express()
-const http=require('http')
+const app = express()
+const http = require('http')
 const server = http.createServer(app)
-const {Server} = require('socket.io')
+const { Server } = require('socket.io')
 const io = new Server(server)
 const httpPort = 3000
 
-io.on('connection', socket => {
-  console.log("new connection ", socket.id)
-  socket.on('message', (message)=>
-    io.emit('newMessage', socket.id + ': ' + message)
-  )
+io.on('connection', (socket) => {
+  const timestamp = new Date()
+  console.log('new connection ', socket.id)
+  socket.broadcast.emit('newMessage', {
+    time: timestamp,
+    msg: socket.id + ' joined'
+  })
+
+  socket.on('message', (message) => {
+    io.emit('newMessage', { time: timestamp, msg: socket.id + ': ' + message })
+  })
 })
 
 app.get('/', (request, response) => {
